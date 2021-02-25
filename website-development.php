64fc15ae241e97b54b8740f93e2e79eed8c382c6
pradhanitservices <pradhanitservices@gmail.com>
2021-02-25 20:26:17 +0530

    web page add

---
+++ website-development.php
@@ -0,0 +1,314 @@
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>Website Development Company in Patna | Website Development Company</title>
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="assets/css/bootstrap.min.css" >
+    <!-- Icon -->
+    <link rel="stylesheet" href="assets/fonts/line-icons.css">
+    <!-- Slicknav -->
+    <link rel="stylesheet" href="assets/css/slicknav.css">
+    <!-- Owl carousel -->
+    <link rel="stylesheet" href="assets/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/css/owl.theme.css">
+    
+    <link rel="stylesheet" href="assets/css/magnific-popup.css">
+    <link rel="stylesheet" href="assets/css/nivo-lightbox.css">
+    <!-- Animate -->
+    <link rel="stylesheet" href="assets/css/animate.css">
+    <!-- Main Style -->
+    <link rel="stylesheet" href="assets/css/main.css">
+    <!-- Responsive Style -->
+    <link rel="stylesheet" href="assets/css/responsive.css">
+
+  </head>
+  <body>
+
+  <header id="header-wrap">
+    <!-- Navbar Start -->
+    <nav class="navbar navbar-expand-md bg-inverse fixed-top scrolling-navbar">
+      <div class="container">
+        <a href="https://pradhanitservices.com/" class="navbar-brand"><img src="assets/img/logo.png" alt="logo" style="width:145px;height:50px;"></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse"
+          aria-expanded="false" aria-label="Toggle navigation">
+          <i class="lni-menu"></i>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarCollapse">
+          <ul class="navbar-nav mr-auto w-100 justify-content-end clearfix">
+            <li class="nav-item active">
+              <a class="nav-link" href="https://pradhanitservices.com/">
+                Home
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://pradhanitservices.com/#feature">
+                About
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://pradhanitservices.com/#services">
+                Services
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://pradhanitservices.com/#testimonial">
+                Testimonial
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#blog">
+                Blog
+              </a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="https://pradhanitservices.com/#contact">
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- Navbar End -->
+
+    <!-- Page header Start -->
+    <section class="page-header">
+      <div class="container">      
+        <div class="row justify-content-md-center">
+          <div class="col-md-12">
+            <div class="breadcrumb-wrapper text-center">
+              <h2>Website Development</h2>
+              <p><a href="https://pradhanitservices.com/#services">Services</a> / Website Development</p>
+            </div>
+          </div>
+        </div> 
+      </div> 
+    </section>
+    <!-- Page header End -->
+    
+
+  <!-- web page start  -->
+   <div id="blog-single">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-12 col-md-12 col-xs-12">
+            <div class="blog-post">
+              <div class="post-thumb">
+		<img src="assets/img/blog/blog-1-big.jpg" alt="">
+              </div>
+
+
+<!-- Full Stack Web Development Services-->
+  <section id="services" class="section-padding bg-gray">
+    <div class="container">
+      <div class="section-header text-center">
+        <h1 class="section-title wow fadeInDown" data-wow-delay="0.6s">Full Stack Web Development Services</h1>
+        <p style="color:#3B6182">We give a large group of website development services for worldwide customers. we are an outstanding website development company in India,  Investigate the wide scope of web development services we have on offer:</p>
+      </div>
+      <div class="row">
+       <!-- Services item -->
+        <div class="col-md-6 col-lg-4 col-xs-12">
+          <div class="services-item wow fadeInRight" data-wow-delay="0.6s">
+            <div class="services-content">
+              <h5>Web Designing</a></h5>
+              <p style="color:#000000" ; align="justify">We are the best website designing company in Patna. Our web designs and work by our website specialists are most acknowledged more and more due to our quality work. Our quality work expands the number of visitors to our client's website. In the event that you are looking through the best website designing company in Patna, Contact us.</p>
+            </div>
+          </div>
+        </div>
+        <!-- Services item -->
+        <div class="col-md-6 col-lg-4 col-xs-12">
+          <div class="services-item wow fadeInRight" data-wow-delay="0.6s">
+            <div class="services-content">
+              <h5>Web Development</h5>
+              <p style="color:#000000" ; align="justify">We have the most creative team of web development. We have set our self as the best website development company in Patna (Bihar), which offers sustained website services for your business. we develop all website is user friendly, responsive, robust, and flexible structure. so, if you are searching for a web development company, contact us.</p>
+            </div>
+          </div>
+        </div>
+        <!-- Services item -->
+        <div class="col-md-6 col-lg-4 col-xs-12">
+          <div class="services-item wow fadeInRight" data-wow-delay="0.6s">
+            <div class="services-content">
+              <h5>Web Hosting</h5>
+              <p style="color:#000000"; align="justify">We are at the top of the web hosting company in Patna, Bihar. We offer a prevalent platform for Windows-based website hosting. We give plans as reasonable to your personal, e-commerce, and business websites incorporate scripting (PHP, ASP, and so forth) and database (MySQL, SQL worker, and so on) ability.</p>
+            </div>
+          </div>
+        </div>
+  <!-- Services item -->
+        <div class="col-md-6 col-lg-4 col-xs-12">
+          <div class="services-item wow fadeInRight" data-wow-delay="0.6s">
+            <div class="services-content">
+              <h5>Website Maintance</h5>
+              <p style="color:#000000" ; align="justify">Best Website Maintenance company situated in Patna, India. Our services cost-adequately within time requirements. Our point is to keep your website secure and exceptional with the goal that your site would not be a test for your business development. Our in-house developed platform makes us accomplish our goal with a simple to the selected process.</p>
+            </div>
+          </div>
+        </div>
+  <!-- Services item -->
+        <div class="col-md-6 col-lg-4 col-xs-12">
+          <div class="services-item wow fadeInRight" data-wow-delay="0.6s">
+            <div class="services-content">
+              <h5>Software Development</h5>
+              <p style="color:#000000" ; align="justify">Software Development is one of the fundamental highlights of a developing industry. We are one of the chief IT industries in this field of software development and a standout amongst software development companies in Patna, Bihar. We have the leading edge of serving over 4 years in the Software Development industry.</p>
+            </div>
+          </div>
+        </div>
+  <!-- Services item -->
+        <div class="col-md-6 col-lg-4 col-xs-12">
+          <div class="services-item wow fadeInRight" data-wow-delay="0.6s">
+            <div class="services-content">
+              <h5>Upgradation</h5>
+              <p style="color:#000000" ; align="justify">A good website advances your brand, work, and mission. Upgrading a website implies carrying it to the front line of technology for ideal outcomes and expanded revenue & profits. So, if you need to redesign or upgrade your current website technology or move current web technology to some other platform, you can reach us.</p>
+            </div>
+          </div>
+        </div>
+</div>
+</div>
+</div>
+
+ <section id="services" class="section-padding bg-gray">
+ <div class="container">
+<div class="section-header text-center">
+        <h2 class="section-title wow fadeInDown" data-wow-delay="0.6s">WHY YOU CHOOSE US FOR WEB DESIGN AND DEVELOPMENT?</h2>
+<p style="color:#000000" ; align="justify">We are the one-stop destination for completely customized and responsive web design & development services. Our website design specialists and developers endeavor and research to explore the full-stack web development services that will bring impressive changes to your brand image. we are majorly focusing on Clean Coding, SEO Friendly, Great UI / UX, Responsive Design, Fully Dynamic and Proven Approach etc. We design and develop websites that suit the necessities of each business person. Each and every website has its own application. it's very well might be a greater amount of information arranged, or to show items or most likely even sell them. we generally make hearty, robust, secure, custom web application with high versatility.</br></br>As per the need for the product, we utilize open-source innovation and technology to build the product with top-notch standards. Some of the websites should be upgradation on a much regular premise and a few are updated once in a while. In view of such business requirements, we have our casket loaded up with practically a wide range of in fact reasonable websites for various business purposes.Our fully trained, exceptionally talented web designers and developers received a client-focused methodology by updating themselves to the most recent web technology like ReactJS, AngularJS, NodeJS, Laravel, Magento 2.0 and numerous others.
+</p>
+</div>
+</div>
+
+ <div class="container">
+      <div class="section-header text-center">
+        <h1 class="section-title wow fadeInDown" data-wow-delay="0.6s">Our Website Development Technology</h1>
+ 	<p style="color:#3B6182"> As India's leading website development company, Pradhan iT Services provides high-performance Website developments to our customers around the world because our development team has in-depth expertise in various advanced website technologies and always try to deilver the Porject on time after full of testing.</p>
+</div>
+</div>
+<div class="container-fluid text-center my-3 bg-light-gray">
+    <div id="recipeCarousel" class="carousel slide w-100" data-ride="carousel">
+      <div class="carousel-inner w-100" role="listbox">
+        <div class="carousel-item row active">
+          <div class="col-2 float-left"><img class="img-fluid" src="assets/img/technologies/html.png" alt="HTML"></div>
+          <div class="col-2 float-left"><img class="img-fluid" src="assets/img/technologies/css.png" alt="CSS"></div>
+          <div class="col-2 float-left"><img class="img-fluid" src="assets/img/technologies/jquery.png" alt="jquery"></div>
+          <div class="col-2 float-left"><img class="img-fluid" src="assets/img/technologies/bootstrap.png" alt="bootstrap"></div>
+          <div class="col-2 float-left"><img class="img-fluid" src="assets/img/technologies/php.png" alt="php"></div>
+          <div class="col-2 float-left"><img class="img-fluid" src="assets/img/technologies/wordpress.png" alt="wordpress"></div>
+</div>
+</div>
+</div>
+</div>
+        	<div class="container-fluid text-center my-3 bg-light-gray">
+    <div id="recipeCarousel" class="carousel slide w-100" data-ride="carousel">
+      <div class="carousel-inner w-100" role="listbox">
+        <div class="carousel-item row active">
+          <div class="col-2 float-left"><img class="img-fluid" src="assets/img/technologies/angular.png" alt="angular"></div>
+          <div class="col-2 float-left"><img class="img-fluid" src="assets/img/technologies/react.png" alt="react"></div>
+          <div class="col-2 float-left"><img class="img-fluid" src="assets/img/technologies/node.png" alt="node"></div>
+          <div class="col-2 float-left"><img class="img-fluid" src="assets/img/technologies/js.png" alt="js"></div>
+          <div class="col-2 float-left"><img class="img-fluid" src="assets/img/technologies/laravel.png" alt="laravel"></div>
+          <div class="col-2 float-left"><img class="img-fluid" src="assets/img/technologies/mysql.png" alt="mysql"></div>
+</div>
+</div>
+</div>
+</div> 
+
+ <div>
+<p style="color:#000000"><b>Note: -</b> <a href="https://pradhanitservices.com/">Pradhan iT Services</a> are here to assist you in developing, upgradation and maintaining your Website. You will contact us any time at <a href="mailto:info@pradhanitservices.com">info@pradhanitservices.com</a> with any inquiries regarding developing, maintenance, upgradation of your website. Our business developer will contact you and response on your inquiries as soon as possible.</p>
+</div>
+
+	    </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- WEB Page End  -->
+
+     <!-- Copyright Section Start Footer -->
+  <div class="copyright">
+    <div class="container">
+      <div class="row">
+
+        <div class="col-lg-4 col-md-3 col-xs-12">
+          <div>
+            <h4>Contact Us</h4>
+            <h6 style="color:#3B6182"><b>ADDRESS :-</b></h6>
+            <h6>3rd floor, Anjir Niwas, Kumhrar, Patna, 800026</h6>
+            <ul>
+              <li>
+                <h6 style="color:#3B6182"><b>Phone :-</b></h6>
+              <li>
+                <h6><a href="tel:+91 9835309230"> +91-9835309230</a></h6>
+              </li>
+              <li>
+                <h6 style="color:#3B6182"><b>Email :-</b></h6>
+              <li>
+                <h6><a href="mailto:info@pradhanitservices.com">info@pradhanitservices.com</a></h6>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-4 col-md-3 col-xs-12">
+          <div>
+            <h4>Services</h4>
+            <ul>
+              <li><a href="#services">Pay Per Click</a></li>
+              <li><a href="#services">Website Devlopment</a></li>
+              <li><a href="#services">Ecommerce Branding</a></li>
+              <li><a href="#services">Content Marketing</a></li>
+              <li><a href="#services">Social Media Marketing</a></li>
+              <li><a href="#services">Search Engine Optimization</a></li>
+
+          </div>
+        </div>
+
+        <div class="col-lg-4 col-md-4 col-xs-12">
+          <div class="social-icon text-left">
+            <a class="facebook" href="https://www.facebook.com/pradhanitservices"><i class="lni-facebook-filled" alt="facebook"></i></a>
+            <a class="twitter" href="https://twitter.com/PradhanServices"><i class="lni-twitter-filled" alt="twitter"></i></a>
+            <a class="instagram" href="https://www.instagram.com/pradhan_it_services/"><i class="lni-instagram-filled"
+                alt="instagram"></i></a>
+            <a class="linkedin" href="https://www.linkedin.com/company/pradhanitservices/"><i class="lni-linkedin-filled"
+                alt="linkedin"></i></a>
+            <a class="instagram" href="https://www.youtube.com/channel/UCwdk2s6WV3d-YQrGPK0ckRg"><img src="assets/img/testimonial/img.png"
+                alt="Youtube"></a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- Copyright Section Footer End -->
+
+  <!-- Go to Top Link -->
+  <a href="#" class="back-to-top">
+    <i class="lni-arrow-up"></i>
+  </a>
+
+  <!-- Preloader -->
+  <div id="preloader">
+    <div class="loader" id="loader-1"></div>
+  </div>
+  <!-- End Preloader -->
+    
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/js/jquery-min.js"></script>
+    <script src="assets/js/popper.min.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/owl.carousel.min.js"></script>
+    <script src="assets/js/jquery.mixitup.js"></script>
+    <script src="assets/js/wow.js"></script>
+    <script src="assets/js/jquery.nav.js"></script>
+    <script src="assets/js/scrolling-nav.js"></script>
+    <script src="assets/js/jquery.easing.min.js"></script>
+    <script src="assets/js/jquery.counterup.min.js"></script>  
+    <script src="assets/js/nivo-lightbox.js"></script>     
+    <script src="assets/js/jquery.magnific-popup.min.js"></script>     
+    <script src="assets/js/waypoints.min.js"></script>   
+    <script src="assets/js/jquery.slicknav.js"></script>
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/form-validator.min.js"></script>
+    <script src="assets/js/contact-form-script.min.js"></script>
+      
+  </body>
+</html>
